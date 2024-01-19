360c61c81b714293664a2d4503176611ab983fec
Phil Howard <github@gadgetoid.com>
2024-01-19 09:45:23 +0000

    Experimental dual MSC.

--- ports/rp2/modules/_boot_fat.py
+++ ports/rp2/modules/_boot_fat.py
@@ -1,15 +1,26 @@
 import vfs
 import machine, rp2
 
+# 1441792
+bdev_root = rp2.Flash(start=0, len=4096 * 100)
+bdev_storage = rp2.Flash(start=4096 * 100, len=4096 * 250)
 
-# Try to mount the filesystem, and format the flash if it doesn't exist.
-bdev = rp2.Flash()
 try:
-    vfs.mount(vfs.VfsFat(bdev), "/")
+    vfs_root = vfs.VfsFat(bdev_root)
+    vfs.mount(vfs_root, "/", readonly=True)
 except:
-    vfs.VfsFat.mkfs(bdev)
-    msc = vfs.VfsFat(bdev)
-    msc.label("RP2_MSC")
-    vfs.mount(msc, "/")
+    vfs.VfsFat.mkfs(bdev_root)
+    vfs_root = vfs.VfsFat(bdev_root)
+    vfs_root.label("Root")
+    vfs.mount(vfs_root, "/", readonly=True)
 
-del vfs, bdev
+try:
+    vfs_storage = vfs.VfsFat(bdev_storage)
+    vfs.mount(vfs_storage, "/storage")
+except:
+    vfs.VfsFat.mkfs(bdev_storage)
+    vfs_storage = vfs.VfsFat(bdev_storage)
+    vfs_storage.label("Storage")
+    vfs.mount(vfs_storage, "/storage")
+
+del bdev_root, bdev_storage, vfs_storage, vfs_root

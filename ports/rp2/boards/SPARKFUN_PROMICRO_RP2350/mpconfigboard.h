4e814d1806091116faa527f6c61602c8031aa3c2
Kirk Benell <kirk.benell@sparkfun.com>
2024-08-28 15:56:03 -0600

    added freq for psram, and a few comments

--- ports/rp2/boards/SPARKFUN_PROMICRO_RP2350/mpconfigboard.h
+++ ports/rp2/boards/SPARKFUN_PROMICRO_RP2350/mpconfigboard.h
@@ -11,8 +11,14 @@
 #define MICROPY_HW_UART1_CTS (10)
 #define MICROPY_HW_UART1_RTS (11)
 
-#define MICROPY_HW_PSRAM_CS_PIN (19)
 
+// PSRAM things
+
+// Enable PSRAM
 #define MICROPY_HW_ENABLE_PSRAM (1)
+// CS Pin for the boards PSRAM
+#define MICROPY_HW_PSRAM_CS_PIN (19)
+// PSRAM max frequency is based on VDD (see datasheet) - SparkFun Pro Micro RP2350 uses 3.3V => 109MHz
+#define MICROPY_HW_PSRAM_MAX_SCK_HZ (109*1000*1000)
 
 // NeoPixel data GPIO25, power not toggleable

d8a9f08178c056a433351249a0a8db51c8a2bf48
Mike Bell <mdb036@gmail.com>
2024-11-17 14:59:02 +0000

    ports/rp2: PSRAM linker script.

--- ports/rp2/rp2_psram.h
+++ ports/rp2/rp2_psram.h
@@ -3,8 +3,6 @@
 #ifndef MICROPY_INCLUDED_RP2_MACHINE_PSRAM_H
 #define MICROPY_INCLUDED_RP2_MACHINE_PSRAM_H
 
-#define PSRAM_LOCATION _u(0x11000000)
-
 extern size_t psram_init(uint cs_pin);
 
 #endif

ec59e4a3e4c2366144d167bed4362e359751fbaf
Phil Howard <github@gadgetoid.com>
2025-06-17 14:07:39 +0100

    rp2: HACK: Set FAT label.

--- ports/rp2/modules/_boot_fat.py
+++ ports/rp2/modules/_boot_fat.py
@@ -5,9 +5,13 @@ import machine, rp2
 # Try to mount the filesystem, and format the flash if it doesn't exist.
 bdev = rp2.Flash()
 try:
-    vfs.mount(vfs.VfsFat(bdev), "/")
+    fat = vfs.VfsFat(bdev)
+    fat.label("Badger2350")
+    vfs.mount(fat, "/")
 except:
     vfs.VfsFat.mkfs(bdev)
-    vfs.mount(vfs.VfsFat(bdev), "/")
+    fat = vfs.VfsFat(bdev)
+    fat.label("Badger2350")
+    vfs.mount(fat, "/")
 
 del vfs, bdev

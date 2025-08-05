676a0ead71f0b34811373165653f53af78d0581d
Phil Howard <github@gadgetoid.com>
2025-08-05 11:10:17 +0100

    Add a UNIX variant with a fenster graphical window.

---
+++ extmod/modfenster.c
@@ -0,0 +1,103 @@
+#include "py/runtime.h"
+#include "fenster.h"
+
+#define W 320
+#define H 240
+
+typedef struct _modfenster_obj_t {
+    mp_obj_base_t base;
+    struct fenster *f;
+} modfenster_obj_t;
+
+struct fenster f = {
+    .title = "MicroPython",
+    .width = W,
+    .height = H,
+    .scale = 2
+};
+
+static mp_obj_t modfenster_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *args) {
+    modfenster_obj_t *self = mp_obj_malloc_with_finaliser(modfenster_obj_t, type);
+
+    f.buf = m_tracked_calloc(W * H, sizeof(uint32_t));
+    self->f = &f;
+
+    fenster_open(self->f);
+
+    return MP_OBJ_FROM_PTR(self);
+}
+
+
+static mp_obj_t modfenster_time(mp_obj_t self_in) {
+    (void)self_in;
+    return mp_obj_new_int_from_ll(fenster_time());
+}
+static MP_DEFINE_CONST_FUN_OBJ_1(modfenster_time_obj, modfenster_time);
+
+
+static mp_obj_t modfenster_sleep(mp_obj_t self_in, mp_obj_t time_in) {
+    (void)self_in;
+    fenster_sleep(mp_obj_get_ll(time_in));
+    return mp_const_none;
+}
+static MP_DEFINE_CONST_FUN_OBJ_2(modfenster_sleep_obj, modfenster_sleep);
+
+
+static mp_obj_t modfenster_loop(mp_obj_t self_in) {
+    modfenster_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    return mp_obj_new_int(fenster_loop(self->f));
+}
+static MP_DEFINE_CONST_FUN_OBJ_1(modfenster_loop_obj, modfenster_loop);
+
+
+static mp_obj_t modfenster_pixel(mp_obj_t self_in, mp_obj_t xy_in, mp_obj_t c_in) {
+    modfenster_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    mp_obj_tuple_t *xy = MP_OBJ_TO_PTR(xy_in);
+    int x = mp_obj_get_int(xy->items[0]);
+    int y = mp_obj_get_int(xy->items[1]);;
+    fenster_pixel(self->f, x, y) = mp_obj_get_int(c_in);
+    return mp_const_none;
+}
+static MP_DEFINE_CONST_FUN_OBJ_3(modfenster_pixel_obj, modfenster_pixel);
+
+
+static mp_obj_t modfenster__del__(mp_obj_t self_in) {
+    modfenster_obj_t *self = MP_OBJ_TO_PTR(self_in);
+    fenster_close(self->f);
+    m_tracked_free(self->f->buf);
+    return mp_const_none;
+}
+static MP_DEFINE_CONST_FUN_OBJ_1(modfenster__del___obj, modfenster__del__);
+
+
+static const mp_rom_map_elem_t modfenster_locals_dict_table[] = {
+    { MP_ROM_QSTR(MP_QSTR_pixel), MP_ROM_PTR(&modfenster_pixel_obj) },
+    { MP_ROM_QSTR(MP_QSTR_loop), MP_ROM_PTR(&modfenster_loop_obj) },
+    { MP_ROM_QSTR(MP_QSTR_time), MP_ROM_PTR(&modfenster_time_obj) },
+    { MP_ROM_QSTR(MP_QSTR_sleep), MP_ROM_PTR(&modfenster_sleep_obj) },
+    { MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&modfenster__del___obj) },
+};
+static MP_DEFINE_CONST_DICT(modfenster_locals_dict, modfenster_locals_dict_table);
+
+
+MP_DEFINE_CONST_OBJ_TYPE(
+    type_Fenster,
+    MP_QSTR_Fenster,
+    MP_TYPE_FLAG_NONE,
+    make_new, modfenster_make_new,
+    locals_dict, &modfenster_locals_dict
+);
+
+static const mp_rom_map_elem_t modfenster_globals_table[] = {
+    { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_fenster) },
+    { MP_ROM_QSTR(MP_QSTR_Fenster),  MP_ROM_PTR(&type_Fenster) },
+};
+static MP_DEFINE_CONST_DICT(modfenster_globals, modfenster_globals_table);
+
+// Define module object.
+const mp_obj_module_t modfenster = {
+    .base = { &mp_type_module },
+    .globals = (mp_obj_dict_t *)&modfenster_globals,
+};
+
+MP_REGISTER_MODULE(MP_QSTR_fenster, modfenster);
---
+++ ports/unix/variants/fenster/mpconfigvariant.mk
@@ -0,0 +1,18 @@
+# This is the default variant when you `make` the Unix port.
+
+CFLAGS += \
+	-I$(TOP)/lib/fenster \
+
+ifeq ($(OS),Windows_NT)
+	LDFLAGS += -lgdi32
+else
+	UNAME_S := $(shell uname -s)
+	ifeq ($(UNAME_S),Darwin)
+		LDFLAGS += -framework Cocoa -framework AudioToolbox
+	else
+		LDFLAGS += -lX11
+	endif
+endif
+
+FROZEN_MANIFEST ?= $(VARIANT_DIR)/manifest.py
+SRC_C += $(TOP)/extmod/modfenster.c

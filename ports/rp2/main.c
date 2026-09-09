07bcedb32d9625f88573a184bfe63d8aec733d80
Phil Howard <github@gadgetoid.com>
2026-09-09 12:22:21 +0100

    ports/rp2: Only set an initial time if the AON timer is not running.
    Only set the initial time when the timer is not already running, mirroring
    machine_rtc.c behaviour when starting the timer.
    
    main() unconditionally started the AON timer at 2021-01-01, discarding any
    time previously set from the host or by NTP. On RP2350 the timer lives in the
    always-on power domain and keeps counting through a reset or a POWMAN
    low-power state.
    
    Signed-off-by: Phil Howard <github@gadgetoid.com>

--- ports/rp2/main.c
+++ ports/rp2/main.c
@@ -139,9 +139,11 @@ int main(int argc, char **argv) {
     #endif
 
     // Start and initialise the RTC
-    struct timespec ts = { 0, 0 };
-    ts.tv_sec = timeutils_seconds_since_epoch(2021, 1, 1, 0, 0, 0);
-    aon_timer_start(&ts);
+    if (!aon_timer_is_running()) {
+        struct timespec ts = { 0, 0 };
+        ts.tv_sec = timeutils_seconds_since_epoch(2021, 1, 1, 0, 0, 0);
+        aon_timer_start(&ts);
+    }
     mp_hal_time_ns_set_from_rtc();
 
     // Initialise stack extents and GC heap.
